f688e6c552cb2803bff5c30b0963d4c78dd4278c
martinaveljanovska <martina.veljanovska@gmail.com>
2020-04-22 12:29:35 +0200

    new function for showing assets

--- js/paywall-scr.js
+++ js/paywall-scr.js
@@ -1,101 +1,96 @@
-var paywall = new InplayerPaywall('1f4cfc0e-7bfb-4aeb-badb-0197da2eba6b', [
+var paywall = new InplayerPaywall("1f4cfc0e-7bfb-4aeb-badb-0197da2eba6b", [
 	{
 		id: 96335,
-		options:
-		{
+		options: {
 			noPreview: true,
-			noInject: true
-		}
+			noInject: true,
+		},
 	},
 	{
 		id: 96126,
-		options:
-		{
+		options: {
 			noPreview: true,
-			noInject: true
-		}
-	}
+			noInject: true,
+		},
+	},
 ]);
 
-
-const checkAccess = a => {
-	console.log('inside checkAccess');
+const checkAccess = (a) => {
+	console.log("inside checkAccess");
 	// console.log(a)
 
 	if (a.hasAccess) {
-		console.log('inside checkAccess hasAccess');
+		console.log("inside checkAccess hasAccess");
 
 		let assetId = a.asset.id;
-		let accessedAsset = $('body').find(`[data-id="${assetId}"]`);
-		console.log(`${assetId} has access and button is hidden`)
+		let accessedAsset = $("body").find(`[data-id="${assetId}"]`);
+		console.log(`${assetId} has access and button is hidden`);
 		accessedAsset.hide();
 	}
-
-}
+};
 
 const reloadAccess = function (a) {
 	if (a.hasAccess === false) {
 		let assetId = a.asset.id;
-		let accessedAsset = $('body').find(`[data-id="${assetId}"]`);
-		console.log(`${assetId} doesn't have access and button is shown once again`)
+		let accessedAsset = $("body").find(`[data-id="${assetId}"]`);
+		console.log(
+			`${assetId} doesn't have access and button is shown once again`
+		);
 		accessedAsset.show();
 	}
-}
+};
 
+// $('.js-buy-96126').on('click', () => {
+// 	paywall.showPaywall({
+// 		asset: {
+// 			assetId: 96126
+// 		}
+// 	});
+// })
 
-$('.js-buy-96126').on('click', () => {
+// $('.js-buy-96335').on('click', () => {
+// 	paywall.showPaywall({
+// 		asset: {
+// 			assetId: 96335
+// 		}
+// 	});
+// })
+
+$(".js-buy").on("click", () => {
+	let assetId = $(this).data("id");
+
+	console.log(typeof assetId);
 	paywall.showPaywall({
 		asset: {
-			assetId: 96126
-		}
+			assetId: 96335,
+		},
 	});
-})
-
-
-$('.js-buy-96335').on('click', () => {
-	paywall.showPaywall({
-		asset: {
-			assetId: 96335
-		}
-	});
-})
-
-paywall.on('access', (e, a) => {
-
-	console.log('access checked')
-	checkAccess(a);
-	setTimeout(() => { reloadAccess(a) }, 60000);
-
-
 });
 
-
-
-
-
+paywall.on("access", (e, a) => {
+	console.log("access checked");
+	checkAccess(a);
+	setTimeout(() => {
+		reloadAccess(a);
+	}, 60000);
+});
 
 $(function () {
-
-
-	var donateButtons = $('.js-inplayer-donate-button');
+	var donateButtons = $(".js-inplayer-donate-button");
 
 	donateButtons.each(function () {
-
 		var currentButton = $(this);
 
-		let assetId = currentButton.data('id');
+		let assetId = currentButton.data("id");
 
 		$.ajax({
 			url: `https://services.inplayer.com/items/${assetId}`,
 			success: function (resp) {
-
 				let buttonLabel = resp.metahash.preview_button_label;
 				currentButton.html(buttonLabel);
-			}
+			},
 		});
-	})
-
-
+	});
 
 	$(".inplayer-paywall-logout").parent().hide();
 	paywall.on("authenticated", function () {
