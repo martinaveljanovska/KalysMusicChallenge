d47abdfe95819f71b170ece2c7e61777f9171678
Stef Naskova <snaskova094@gmail.com>
2020-04-30 13:00:09 +0200

    updated bands info

--- js/bands.js
+++ js/bands.js
@@ -1,10 +1,10 @@
 export default [
     {
         "id": 1,
-        "title": 'Video title one',
-        "image": 'img/thumb/video-one.jpg',
-        "video": `<iframe width="610" height="339" frameborder="0" allowfullscreen
-        src="https://www.youtube.com/embed/LOgsQZYt8iI"></iframe>`,
+        "assetId": 96126,
+        "title": 'MusaiQ',
+        "image": 'img/thumb/musaiq.jpg',
+        "video": `<iframe width="560" height="315" src="https://www.youtube.com/embed/PQmm_OwqK6A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
         "descriptionEN": `MusaiQ entered the audience&#39;s sonic map in late 2015. A self- evident, mosaiq-like
 		musicianship is the life force of this unusual collective. The music they create is an alloy of
 		styles tied together by a specific thread that is actually the heart of this group, whether it be
@@ -36,26 +36,49 @@ export default [
     },
     {
         "id": 2,
-        "title": 'Video title two',
-        "image": 'img/thumb/video-one.jpg',
-        "video": `<iframe width="610" height="339" frameborder="0" allowfullscreen
-        src="https://www.youtube.com/embed/LOgsQZYt8iI"></iframe>`,
-        'description': `Desc for video two`
+        "assetId": 96335,
+        "title": 'Zulu 3.4',
+        "image": 'img/thumb/zulu.jpg',
+        "video": `<iframe width="560" height="315" src="https://www.youtube.com/embed/Of8oe4FdwYk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+        "descriptionEN": `Zulu 3.4 is an instrumental band from Skopje, founded in March 2018 by three young jazz
+        enthusiasts, whose goal was to create a unique sound through fusion of different styles and
+        bring freshness to the Macedonian music stage. In March 2019 they have promoted their
+        debut album &quot;FM to AM&quot;, which was recorded at the Alshar studio. Their second album
+        &quot;Expandability&quot; was released and promoted in March 2020. Zulu 3.4 consists of three
+        members: Damjan Danevski – drums (Primordial Soup, Skopje Drum Talks), Mario
+        Simonovski – guitar (Pluto’s Doubts) and Martin Dzhorlev – keyboards (Martin Djorlev).`,
+        "descriptionMK": `Зулу 3.4 е инструментален бенд од Скопје, формиран во март 2018 година. Членови на
+        бендот се музички уметници во сферата на џез музиката, чија цел е преку фузија на
+        различни стилови да создадат уникатен звук и да внесат свежина на македонската
+        музичка сцена. Во март 2019 тие го промовираа својот деби албум „FM to AM” ,
+        снимен во студиото Алшар. Вториот албум „Expandability“ беше издаден во март 2020.
+        Зулу 3.4 се состои од три члена: Дамјан Даневски - тапани (Примордијална Манџа,
+        Skopje Drum Talks), Марио Симоновски - гитара (Pluto’s Doubts) и Мартин Џорлев –
+        клавијатури (Martin Djorlev).`
     },
     {
         "id": 3,
-        "title": 'Video title three',
-        "image": 'img/thumb/video-one.jpg',
+        "assetId": 97121,
+        "title": 'Junkyard',
+        "image": 'img/thumb/junkyard.jpg',
         "video": `<iframe width="610" height="339" frameborder="0" allowfullscreen
         src="https://www.youtube.com/embed/LOgsQZYt8iI"></iframe>`,
-        'description': `Desc for video three`
+        "descriptionEN": `Hey, guys! We are Junkyard, a band situated in the capital of North Macedonia, Skopje. Our
+        journey actually begins few years ago, 2017 to be exact. We are a group of young students and
+        musicians who happen to share the same love and passion for the sound of funk and disco
+        music, and also love playing and making music together as friends. Our sound is made under
+        the influence of funk music, we kind of like the 70’s and 80’s a bit too much. As a band, we’ve
+        played at different places throughout Skopje, such as some of the night clubs, pubs, festivals,
+        we’ve also been guests to weddings and private parties.
+        Currently, we’re working on our first album. It’s about time you meet the band: • Lead vocal:
+        Marija Ilievska • Guitar: Filip Popov • Keys: Stefan Aleksovski • Bass: Kristijan Kozhumanovski •
+        Drums: Teodor Teodosievski`,
+        "descriptionMK": `Junkyard е бенд кој доаѓа од Скопје, оформен во 2017тата година од страна на млади
+        студенти/музичари, ентузијасти за фанк и ретро музиката. Звукот на Junkyard e
+        aвтентичен, произлезен под влијание на фанк и ретро музиката од 70тите и 80тите
+        години. Како бенд, имаат настапувано на најразлични настани и во најразлични локали.
+        Моментално работат на нивниот прв авторски албум. Во бендот членуваат: • Вокал:
+        Марија Илиевска • Гитара: Филип Попов • Клавијатури: Стефан Алексовски • Бас:
+        Кристијан Кожумановски • Тапани: Теодор Теодосиевски`
     },
-    {
-        "id": 4,
-        "title": 'Video title four',
-        "image": 'img/thumb/video-one.jpg',
-        "video": `<iframe width="610" height="339" frameborder="0" allowfullscreen
-        src="https://www.youtube.com/embed/LOgsQZYt8iI"></iframe>`,
-        'description': `Desc for video four`
-    }
-]
+];
